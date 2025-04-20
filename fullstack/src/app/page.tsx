e005ea372d69eca3515ff40229d4301a4c1b0c5a
daisy9542 <daisy_52036@outlook.com>
2025-04-20 22:08:35 +0800

    refactor(fullstack): 调整排行卡片样式

--- fullstack/src/app/page.tsx
+++ fullstack/src/app/page.tsx
@@ -132,28 +132,23 @@ export default function IndexPage() {
           />
         )}
 
-        {/* 左侧 KOL 信息卡片 */}
-        <div className="transparent fixed bottom-16 left-0 top-16 flex h-[calc(100vh-64px)] w-80 flex-col space-y-4 overflow-hidden overflow-y-auto p-4 transition-transform duration-300 ease-in-out will-change-transform">
-          {selectedKol !== null ? <KolInfo /> : null}
-        </div>
-
-        {/* 右侧筛选与排名卡片 */}
+        {/* 左侧筛选与排名卡片 */}
         <div className="relative">
           <Button
             variant="outline"
             onClick={() => setLeftCardsOpen(!leftCardsOpen)}
             className={cn(
-              "absolute right-2 top-6 h-10 w-10 transition-transform duration-300 ease-in-out will-change-transform",
+              "absolute left-2 top-6 h-10 w-10 transition-transform duration-300 ease-in-out will-change-transform",
               leftCardsOpen
-                ? "-mr-2 translate-x-[-320px]"
-                : "mr-2 translate-x-0",
+                ? "-ml-2 translate-x-[320px]"
+                : "translate-x-0",
             )}
           >
             <ChevronRight />
           </Button>
           <div
             className={cn(
-              "transparent fixed bottom-16 right-0 top-16 flex h-[calc(100vh-64px)] flex-col space-y-4 overflow-hidden overflow-y-auto border-none transition-all duration-300 ease-in-out will-change-transform",
+              "transparent fixed bottom-16 left-0 top-16 flex h-[calc(100vh-64px)] flex-col space-y-4 overflow-hidden overflow-y-auto border-none transition-all duration-300 ease-in-out will-change-transform",
               leftCardsOpen ? "w-80 border-r p-4" : "w-0 p-0",
             )}
           >
@@ -164,6 +159,11 @@ export default function IndexPage() {
               </>
             )}
           </div>
+        </div>
+
+        {/* 右侧 KOL 信息卡片 */}
+        <div className="transparent fixed bottom-16 right-0 top-16 flex h-[calc(100vh-64px)] w-80 flex-col space-y-4 overflow-hidden overflow-y-auto p-4 transition-transform duration-300 ease-in-out will-change-transform">
+          {selectedKol !== null ? <KolInfo /> : null}
         </div>
 
         {/* 底部 K 线图 */}

246fc5111bfc80e5dda3845710aec0a4ca1b6e88
daisy9542 <daisy_52036@outlook.com>
2025-06-08 10:17:43 +0800

    style: 调整样式

--- news-graph/src/app/layout.tsx
+++ news-graph/src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Story Line",
+  title: "StoryLine",
   description: "Nothing...",
 };
 
